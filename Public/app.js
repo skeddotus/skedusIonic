7eb5e2601aa8387642eb3b0daea57d87428a7524
kpfein <kpfein@yahoo.com>
2016-01-18 16:35:58 -0600

    stuff

--- Public/app.js
+++ Public/app.js
@@ -20,8 +20,8 @@ angular.module("skedApp", ["ui.router"]).config(function($stateProvider, $urlRou
 //AUTH STATES ////////////////////////////////////////////////////////////////
 		.state('auth', {
 			url: "",
-			templateUrl: "templates/home.html",
-			controller: "homeCtrl",
+			templateUrl: "templates/main.html",
+			controller: "mainCtrl",
 			resolve: {
 				user: function(authService) {
 					return authService.getAuthedUser();
