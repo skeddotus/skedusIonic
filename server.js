5e725d175dc1d1fc016eeeeb0f747d1f001b70e4
Rory McLaughin <rorymcl@gmail.com>
2016-01-25 15:53:25 -0600

    Heroku config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var express = require('express'),
     async = require('async'),
     crypto = require('crypto'),
     mongoUri = require('./Server-assets/Config/database.js'),
-    Secret = require('./Server-assets/Secrets/secrets.js'); 
+    Secret = require('./Server-assets/Secrets/secrets.js');
 
     app.use(cors(), bodyParser.json(), express.static(__dirname + '/Public'));
 
@@ -98,8 +98,8 @@ var requireAuth = function(req, res, next) {
     app.get('/api/user/email/validation/:userID', userServCtrl.validateEmail);
 
 
-app.listen(port, function() {
-    console.log("Listening on port ", port);
+app.listen(process.env.PORT || port, function() {
+    console.log("Listening on port ", process.env.PORT || port);
 });
 
 mongoose.connect(process.env.MONGOLAB_URI || mongoUri.url);
